eb098c36a9f6109b739b342abfdb543b72b28388
code-pilgrimz <253719668+code-pilgrimz@users.noreply.github.com>
2022-12-28 10:56:48 +0900

    api: audit_log controller

---
+++ api/src/controllers/audit_log.js
@@ -0,0 +1,8 @@
+const Store = require("../lib/store");
+const store = new Store();
+const FIELDS = ["action", "actor_id", "target"];
+
+exports.list = (_req, res) => res.json(store.list());
+exports.get = (req, res) => { const row = store.get(req.params.id); return row ? res.json(row) : res.status(404).json({ error: "not found" }); };
+exports.create = (req, res) => { const data = {}; FIELDS.forEach((k) => (data[k] = req.body[k])); res.status(201).json(store.create(data)); };
+exports.remove = (req, res) => { store.remove(req.params.id); res.status(204).end(); };
